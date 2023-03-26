88236f4f917b6bbe0736846e13d4bc1e2b6f0024
RJD72 <100567050+RJD72@users.noreply.github.com>
2023-03-26 16:52:46 -0400

    Delete Final Project directory

--- Final Project/js/script.js
+++
@@ -1,9 +0,0 @@
-const navEl = document.querySelector("nav");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 500) {
-    navEl.classList.add("nav-scrolled");
-  } else if (window.scrollY < 500) {
-    navEl.classList.remove("nav-scrolled");
-  }
-});
